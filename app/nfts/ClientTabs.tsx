aae2fea9f5f6766aad6a1ce6ad036b6d3ed255c1
Dustin <135719141+DustinTurska@users.noreply.github.com>
2024-09-27 07:04:49 -0500

    erc1155.claimTo

--- app/nfts/ClientTabs.tsx
+++ app/nfts/ClientTabs.tsx
@@ -2,6 +2,7 @@
 
 import { Card, Tabs, Tab, Link } from "@nextui-org/react";
 import ClaimTo from "@/components/ClaimTo";
+import ERC1155ClaimTo from "@/components/ERC1155claimTo";
 
 export default function ClientTabs() {
   return (
@@ -21,7 +22,20 @@ export default function ClientTabs() {
           </div>
           <ClaimTo />
         </Tab>
-        <Tab key="ERC1155" title="ERC1155"></Tab>
+        <Tab key="ERC1155" title="ERC1155">
+          <div className="flex justify-center w-full mb-4">
+            <Link
+              isExternal
+              className="flex items-center gap-1 text-current"
+              href="https://github.com/DustinTurska/thirdweb-Engine-Scripts/blob/main/erc1155claimTo.ts"
+              title="thirdweb ERC721.ClaimTo Engine Script"
+            >
+              <span className="text-default-600">See</span>
+              <p className="text-primary">erc1155.claimTo</p>
+            </Link>
+          </div>
+          <ERC1155ClaimTo />
+        </Tab>
       </Tabs>
     </Card>
   );
---
+++ components/ERC1155claimTo.tsx
@@ -0,0 +1,197 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { Button, Link } from "@nextui-org/react";
+import { NFTTransactionResults } from "./NFTTransactionResults";
+import { useActiveAccount } from "thirdweb/react";
+
+interface ClaimResult {
+  queueId: string;
+  status: "Mined ⛏️" | "error";
+  transactionHash?: string;
+  blockExplorerUrl?: string;
+  errorMessage?: string;
+  toAddress: string;
+  tokenId: string;
+  amount: string;
+}
+
+export default function ERC1155ClaimTo() {
+  const [amount, setAmount] = useState("1");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [results, setResults] = useState<ClaimResult[]>([]);
+
+  const activeAccount = useActiveAccount();
+  const [toAddress, setToAddress] = useState("");
+  const [tokenId, setTokenId] = useState("1")
+
+  useEffect(() => {
+    if (activeAccount?.address) {
+      setToAddress(activeAccount.address);
+    }
+  }, [activeAccount]);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!toAddress || !amount) {
+      alert("Please enter both address and amount.");
+      return;
+    }
+
+    const amountNum = parseInt(amount);
+    if (isNaN(amountNum) || amountNum < 1 || amountNum > 5) {
+      alert("Amount must be between 1 and 5.");
+      return;
+    }
+
+    setIsSubmitting(true);
+
+    try {
+      const response = await fetch("/api/claimERC1155", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ receiver: toAddress, tokenId: tokenId, quantity: amount }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const result = await response.json();
+      setResults((prevResults) => [
+        ...prevResults,
+        {
+          ...result,
+          toAddress,
+          amount,
+          status: result.status === "mined" ? "Mined ⛏️" : "error",
+        },
+      ]);
+    } catch (error) {
+      console.error("Error:", error);
+      setResults((prevResults) => [
+        ...prevResults,
+        {
+          queueId: "",
+          status: "error",
+          errorMessage: "Error claiming tokens. Check console for details.",
+          toAddress,
+          tokenId,
+          amount,
+        },
+      ]);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    const numValue = parseInt(value);
+    if (value === "" || (numValue >= 1 && numValue <= 5)) {
+      setAmount(value);
+    }
+  };
+
+  const handleTokenIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    const numValue = parseInt(value);
+    if (value === "" || (numValue >= 1 && numValue <= 5)) {
+      setAmount(value);
+    }
+  };
+
+  return (
+    <div className="bg-black flex flex-col items-center p-4">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
+        <h1 className="text-3xl font-bold mb-6 text-center text-gray-800">
+          Claim ERC1155
+        </h1>
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div className="space-y-4">
+            <div>
+              <label
+                htmlFor="address"
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Address
+              </label>
+              <input
+                id="address"
+                type="text"
+                placeholder="Login to Claim"
+                className="block w-full text-sm text-gray-700 border border-gray-300 rounded-lg p-2.5 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                value={toAddress}
+                onChange={(e) => setToAddress(e.target.value)}
+                required
+                readOnly
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="amount"
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Token Id
+              </label>
+              <input
+                id="tokenId"
+                type="number"
+                min="1"
+                max="5"
+                placeholder="1"
+                className="block w-full text-sm text-gray-700 border border-gray-300 rounded-lg p-2.5 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                value={amount}
+                onChange={handleTokenIdChange}
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="amount"
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Amount
+              </label>
+              <input
+                id="amount"
+                type="number"
+                min="1"
+                max="5"
+                placeholder="1"
+                className="block w-full text-sm text-gray-700 border border-gray-300 rounded-lg p-2.5 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                value={amount}
+                onChange={handleAmountChange}
+                required
+              />
+            </div>
+          </div>
+          <Button
+            type="submit"
+            color="primary"
+            isLoading={isSubmitting}
+            disabled={isSubmitting || !activeAccount}
+            className="w-full"
+          >
+            {isSubmitting ? "Submitting..." : "Claim NFT"}
+          </Button>
+        </form>
+
+        {results.length > 0 && (
+          <div className="mt-8">
+            <NFTTransactionResults results={results} />
+            <Button
+              onClick={() => setResults([])}
+              color="secondary"
+              className="mt-4 w-full"
+            >
+              Clear Results
+            </Button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
