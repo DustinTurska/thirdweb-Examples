75e356286f8e357b53e69284a628bb850169f41f
Dustin <135719141+DustinTurska@users.noreply.github.com>
2024-09-27 08:25:41 -0500

    updates

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "thirdweb Engine Demo",
+  description: "Playground for the most powerful tool in web3",
   navItems: [
     {
       label: "Home",
